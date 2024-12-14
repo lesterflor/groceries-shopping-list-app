6b9ccf02783e47b99ab60e8d6f4d98883cbc1b8b
James Pearce <github@tripleodeon.com>
2024-12-14 12:09:51 -0500

    Emoji in header

--- app/(index)/list/index.tsx
+++ app/(index)/list/index.tsx
@@ -2,18 +2,20 @@ import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
 import { useShoppingListCell } from "@/stores/ShoppingListStore";
 import { Stack, useLocalSearchParams } from "expo-router";
+import { Text } from "react-native";
 import React from "react";
 
 export default function ListScreen() {
   const { listId } = useLocalSearchParams() as { listId: string };
+  const name = useShoppingListCell(listId, "name");
+  const emoji = useShoppingListCell(listId, "emoji");
+  const color = useShoppingListCell(listId, "color");
   return (
     <>
       <Stack.Screen
         options={{
-          headerTitle: useShoppingListCell(listId, "name"),
-          headerLargeStyle: {
-            backgroundColor: useShoppingListCell(listId, "color"),
-          },
+          headerTitle: emoji + " " + name,
+          headerLargeStyle: { backgroundColor: color },
         }}
       />
       <BodyScrollView contentContainerStyle={{ paddingHorizontal: 16 }}>
